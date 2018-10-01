a857eb85855c0a6a4e473480584daa1feb640ebc
Thomas Wong <38015348+ThomasWongMingHei@users.noreply.github.com>
2018-10-01 23:17:49 +0100

    option to upload to gdrive or not
    useful if file too big, copy with hard drive at the spot instead

--- Bloomberg/blgterminal_task.py
+++ Bloomberg/blgterminal_task.py
@@ -2,19 +2,12 @@
 """
 This script is to download data from bloomberg terminal and save as csv or upload to mlab
 Require: 
-Active Bloomberg terminal login
+Active bcmm which is in the BLoomberg folder
 set env variable for PATH to include the folder to blpapi C++ DSK so that blpapi can be imported
-
-Need to refer to QT_pydrive 
-
 set BLPAPI_ROOT=D:\bloomberg_api\blpapi_cpp_3.8.18.1
 set PATH=%PATH%;D:\bloomberg_api\blpapi_cpp_3.8.18.1\bin
-conda install cython
+
 python -m pip install --index-url=https://bloomberg.bintray.com/pip/simple blpapi
-
-pip install pymongo
-pip install git+https://github.com/manahl/arctic.git
-
 pip install --upgrade google-api-python-client oauth2client
 pip install pyDrive
 pip install gspread gspread_dataframe
@@ -26,6 +19,9 @@ import QT_pydrive
 import QT_gspread
 
 from datetime import datetime 
+
+pd.options.display.max_rows = 10000
+pd.options.display.max_columns = 30
 
 Downloadfolder='H:/GitHub/QT_Engineering/CurrentData/'
 Downloadfolder2='H:/GitHub/QT_Engineering/Data/'
@@ -40,7 +36,7 @@ def _prase_name(name):
     return newname
 
 # For each command in recent data request file, run the command
-def download_currentdata():
+def download_currentdata(recentdf):
     
     recentdata=[]
     
@@ -98,8 +94,14 @@ def download_currentdata():
     filename=currentdatafolder+_currentdate()+'bdpdata.csv'
     print(allcurrentbdp)
     allcurrentbdp.to_csv(filename)
-    drive=QT_pydrive.create_pydrive_auth()
-    QT_pydrive.upload_folder_pydrive(drive,currentdatafolder,'Bloomberg_CurrentData')
+
+    gclient=QT_gspread.gspread_client(credentials='H:\GitHub\QT_Engineering\Bloomberg\credentials.json')
+    QT_gspread.list_all_spreadsheet(gclient)
+    recentdf=QT_gspread.sheet2df(gclient,'Bloomberg_recent_data_cfg','Recent_data')
+    print(recentdf)
+
+    return recentdf
+
 
 
 def download_historical_data():
@@ -165,29 +167,43 @@ def download_historical_data():
     drive=QT_pydrive.create_pydrive_auth()
     QT_pydrive.upload_folder_pydrive(drive,currentdatafolder,'Bloomberg_Data')
 
+def upload_data():
+    drive=QT_pydrive.create_pydrive_auth()
+    currentdatafolder=Downloadfolder
+    QT_pydrive.upload_folder_pydrive(drive,currentdatafolder,'Bloomberg_CurrentData')
+    currentdatafolder=Downloadfolder2
+    QT_pydrive.upload_folder_pydrive(drive,currentdatafolder,'Bloomberg_Data')
 
 
-# Read the config file or download from mlab 
-pd.options.display.max_rows = 10000
-pd.options.display.max_columns = 30
-gclient=QT_gspread.gspread_client(credentials='H:\GitHub\QT_Engineering\Bloomberg\credentials.json')
-QT_gspread.list_all_spreadsheet(gclient)
-recentdf=QT_gspread.sheet2df(gclient,'Bloomberg_recent_data_cfg','Recent_data')
-historydf=QT_gspread.sheet2df(gclient,'Bloomberg_historical_data_cfg','Historical_data')
-print(recentdf)
-print(historydf)
+# Read the config file
+def loan_config(gspread=True):
+    if gspread:
+        gclient=QT_gspread.gspread_client(credentials='H:\GitHub\QT_Engineering\Bloomberg\credentials.json')
+        QT_gspread.list_all_spreadsheet(gclient)
+        recentdf=QT_gspread.sheet2df(gclient,'Bloomberg_recent_data_cfg','Recent_data')
+        historydf=QT_gspread.sheet2df(gclient,'Bloomberg_historical_data_cfg','Historical_data')
+        print(recentdf)
+        print(historydf)
+        return{'Recent':recentdf,'History':historydf}
+    else:
+        # load from local file
 
-# Start the bloomberg connection
-bloomberg = blgapi.BLP()
-switch1=raw_input('Download current data? Y/N ')
-switch2=raw_input('Download historical data? Y/N ')
-if switch1=='Y':
-    download_currentdata()
-if switch2=='Y':
-    download_historical_data()
-# Close the bloomberg connection
-bloomberg.closeSession()
+if __name__ == '__main__':
+    taskdf=load_config()
+    # Start the bloomberg connection
+    bloomberg = blgapi.BLP()
+    switch1=raw_input('Download current data? Y/N ')
+    switch2=raw_input('Download historical data? Y/N ')
+    switch3=raw_input('Upload data to Google Drive? Y/N ')
+    if switch1=='Y':
+        download_currentdata(taskdf['Current'])
+    if switch2=='Y':
+        download_historical_data(taskdf['History'])
+    if switch3=='Y':
+        upload_data()
+    # Close the bloomberg connection
+    bloomberg.closeSession()
 
 
-# 
+    # 
 
