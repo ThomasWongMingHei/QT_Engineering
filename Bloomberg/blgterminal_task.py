858ba0325acda44f24cf24a2f7cf60856485bf1d
Thomas Wong <38015348+ThomasWongMingHei@users.noreply.github.com>
2018-10-02 15:25:31 +0100

    Add files via upload

--- Bloomberg/blgterminal_task.py
+++ Bloomberg/blgterminal_task.py
@@ -18,7 +18,7 @@ import blgapi
 import QT_pydrive
 import QT_gspread
 
-from datetime import datetime 
+import datetime 
 
 pd.options.display.max_rows = 10000
 pd.options.display.max_columns = 30
@@ -28,7 +28,7 @@ Downloadfolder2='H:/GitHub/QT_Engineering/Data/'
 
 
 def _currentdate():
-    return datetime.now().strftime("%Y_%m_%d_%H_%M_%S_")
+    return datetime.datetime.now().strftime("%Y_%m_%d_%H_%M_%S_")
 
 def _prase_name(name):
     import re
@@ -52,30 +52,31 @@ def download_currentdata(recentdf):
         dbname=record['dbname']
         dbcollection=record['dbcollection']
         eventtype=record['Eventtype']
-        exchangecode=record['Exchangecode']
+        #exchangecode=record['Exchangecode']
     
         # Download recent data as dataframe 
         if downloadmethod=='bdp' or  downloadmethod=='BDP':
-            try:
-                result=bloomberg.bdp(security, fieldlist)
+            try:               
                 print(security, fieldlist)
-                print(result)
+                result=bloomberg.bdp(security, fieldlist)
+                result['Download_Time']=datetime.datetime.now()
                 recentdata.append(result)
+                print(result)
             except:
                 print('Error ',security)
     
         #Download historical intraday data
         if downloadmethod=='Intraday':
             try:
-                result=bloomberg.blgbar(security,start,end,eventtype,freq)
                 print(security,start,end,eventtype,freq)
+                result=bloomberg.blgbar(security,start,end,eventtype,freq)
                 print(result)
             except:
                 print('Error ',security)
     
             # Save results 
         if sendmethod=='csv':
-            csvpath=_prase_name(security)
+            csvpath=_prase_name(security)+'.csv'
             csvpath=Downloadfolder+_currentdate()+csvpath
             result.to_csv(csvpath)
             # send results through google drive api
@@ -104,7 +105,7 @@ def download_currentdata(recentdf):
 
 
 
-def download_historical_data():
+def download_historical_data(historydf):
 
     # For each command in historical request file, run the command 
     for entry in historydf.iterrows():
@@ -113,30 +114,24 @@ def download_historical_data():
         security=record['GST']
         fieldlist=record['Datafields'].split(',')
         freq=record['Frequency']
-        start=datetime.strptime(record['Start'],'%Y-%m-%d').date()
-        end=datetime.strptime(record['End'],'%Y-%m-%d').date()
+        start=datetime.datetime.strptime(record['Start'],'%Y-%m-%d').date()
+        end=datetime.datetime.strptime(record['End'],'%Y-%m-%d').date()
         sendmethod=record['Location']
         csvpath=record['Filepath']
         dburl=record['dburl']
         dbname=record['dbname']
         dbcollection=record['dbcollection']
         eventtype=record['Eventtype']
-        exchangecode=record['Exchangecode']
+        #exchangecode=record['Exchangecode']
     
     
     
-        # Download recent data as dataframe 
-        if downloadmethod=='bdp' or  downloadmethod=='BDP':
-            result=bloomberg.bdp(security, fieldlist)
-            print(security, fieldlist)
-            print(result)
-        
         # Download historical pricing data 
         # Equities data always split adjusted
         if downloadmethod=='bdh' or downloadmethod=='BDH':
             try:
-                result=bloomberg.bdh(security,fieldlist,start,end,adjustmentSplit=True,periodicity=freq)
                 print(security,start,end,eventtype,freq)
+                result=bloomberg.bdh(security,fieldlist,start,end,adjustmentSplit=True,periodicity=freq)
                 print(result)
             except:
                 result=pd.DataFrame()
@@ -144,13 +139,16 @@ def download_historical_data():
     
         #Download historical intraday data
         if downloadmethod=='Intraday':
-            result=bloomberg.blgbar(security,start,end,eventtype,freq)
-            print(security,start,end,eventtype,freq)
-            print(result)
+            try:
+                print(security,start,end,eventtype,freq)
+                result=bloomberg.blgbar(security,start,end,eventtype,freq)
+                print(result)
+            except:
+                print('Error ',security)
       
         # Save results 
         if sendmethod=='csv':
-            csvpath=_prase_name(security)
+            csvpath=_prase_name(security)+'.csv'
             csvpath=Downloadfolder2+_currentdate()+csvpath
             result.to_csv(csvpath)
             # send results through google drive api
@@ -162,10 +160,6 @@ def download_historical_data():
             db_cm.insert_many(result.to_dict('records'))
             client.close()
     
-    # Upload all the csv files in the directory to google_drive
-    currentdatafolder=Downloadfolder2
-    drive=QT_pydrive.create_pydrive_auth()
-    QT_pydrive.upload_folder_pydrive(drive,currentdatafolder,'Bloomberg_Data')
 
 def upload_data():
     drive=QT_pydrive.create_pydrive_auth()
@@ -176,7 +170,7 @@ def upload_data():
 
 
 # Read the config file
-def loan_config(gspread=True):
+def load_config(gspread=True):
     if gspread:
         gclient=QT_gspread.gspread_client(credentials='H:\GitHub\QT_Engineering\Bloomberg\credentials.json')
         QT_gspread.list_all_spreadsheet(gclient)
@@ -186,6 +180,7 @@ def loan_config(gspread=True):
         print(historydf)
         return{'Recent':recentdf,'History':historydf}
     else:
+        print('')
         # load from local file
 
 if __name__ == '__main__':
@@ -196,7 +191,7 @@ if __name__ == '__main__':
     switch2=raw_input('Download historical data? Y/N ')
     switch3=raw_input('Upload data to Google Drive? Y/N ')
     if switch1=='Y':
-        download_currentdata(taskdf['Current'])
+        download_currentdata(taskdf['Recent'])
     if switch2=='Y':
         download_historical_data(taskdf['History'])
     if switch3=='Y':
